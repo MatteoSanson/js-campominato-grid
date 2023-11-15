1447cbc658514aa2e14c5cfe61424fd14423e8e2
MatteoSanson <matteo.sanson@gmail.com>
2023-11-15 16:24:11 +0100

    feat: griglia con js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+{/* <main>
+    <div class="griglia">
+        <div class="cell"></div>
+    </div>
+</main> */}
 'use strict';
 
-console.log ('sto funzionando');
+function elementoCella(tag, className, content){
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    element.append(content);
+    return element;
+}
+
+const griglia = document.querySelector('.griglia');
+console.log(griglia);
+
+for (let i=1; i<=100; i++) {
+    const mioElemento = elementoCella('div', 'cell', i);
+    griglia.append(mioElemento);
+}
