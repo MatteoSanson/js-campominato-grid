2039d80da1b55cc910fd2475773720a24df11766
MatteoSanson <matteo.sanson@gmail.com>
2023-11-15 17:34:01 +0100

    feat: click alla cella

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
-// 'use strict';
-
-// function elementoCella(tag, className, content){
-//     const element = document.createElement(tag);
-//     element.classList.add(className);
-//     element.append(content);
-//     return element;
-// }
-
-// const griglia = document.querySelector('.griglia');
-// console.log(griglia);
-
-// for (let i=1; i<=100; i++) {
-//     const mioElemento = elementoCella('div', 'cell', i);
-//     griglia.append(mioElemento);
-// }
-
 'use strict';
 
 function elementoCella(tag, className, content) {
     const element = document.createElement(tag);
     element.classList.add(className);
     element.append(content);
+
+    aggiungiEventoClick(element);
+
     return element;
 }
+
+function aggiungiEventoClick(element) {
+    element.addEventListener('click', function() {
+        element.classList.add('clicked');
+        const cellNumber = element.textContent;
+        console.log('Cella cliccata:', cellNumber);
+    });
+}
+
+let grigliaGenerata = false;
 
 const griglia = document.querySelector('.griglia');
 const playButton = document.querySelector('.play');
@@ -31,10 +27,12 @@ const startMessage = document.querySelector('.start-message');
 playButton.addEventListener('click', function() {
     startMessage.style.display = 'none';
 
-    for (let i = 1; i <= 100; i++) {
-        const mioElemento = elementoCella('div', 'cell', i);
-        griglia.append(mioElemento);
-    }
 
-    playButton.disabled = true;
+    if (!grigliaGenerata) {
+        for (let i = 1; i <= 100; i++) {
+            const mioElemento = elementoCella('div', 'cell', i);
+            griglia.append(mioElemento);
+        }
+        grigliaGenerata = true;
+    }
 });
