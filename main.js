709db704455281f4fa11c24df44c295a0257a422
MatteoSanson <matteo.sanson@gmail.com>
2023-11-15 16:52:57 +0100

    feat: tasto play

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-{/* <main>
-    <div class="griglia">
-        <div class="cell"></div>
-    </div>
-</main> */}
+// 'use strict';
+
+// function elementoCella(tag, className, content){
+//     const element = document.createElement(tag);
+//     element.classList.add(className);
+//     element.append(content);
+//     return element;
+// }
+
+// const griglia = document.querySelector('.griglia');
+// console.log(griglia);
+
+// for (let i=1; i<=100; i++) {
+//     const mioElemento = elementoCella('div', 'cell', i);
+//     griglia.append(mioElemento);
+// }
+
 'use strict';
 
-function elementoCella(tag, className, content){
+function elementoCella(tag, className, content) {
     const element = document.createElement(tag);
     element.classList.add(className);
     element.append(content);
@@ -13,9 +25,16 @@ function elementoCella(tag, className, content){
 }
 
 const griglia = document.querySelector('.griglia');
-console.log(griglia);
+const playButton = document.querySelector('.play');
+const startMessage = document.querySelector('.start-message');
 
-for (let i=1; i<=100; i++) {
-    const mioElemento = elementoCella('div', 'cell', i);
-    griglia.append(mioElemento);
-}
+playButton.addEventListener('click', function() {
+    startMessage.style.display = 'none';
+
+    for (let i = 1; i <= 100; i++) {
+        const mioElemento = elementoCella('div', 'cell', i);
+        griglia.append(mioElemento);
+    }
+
+    playButton.disabled = true;
+});
